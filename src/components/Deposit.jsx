c50586eb82e71310079da8e5542985642a5e018a
serhii <meksikann@gmail.com>
2019-12-12 16:50:36 +0200

    new arch

---
+++ src/components/Deposit.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Deposit extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+
+    }
+
+    render() {
+        return (
+            <div className='app-container'>
+            Deposit page
+            </div>
+        )
+    }
+}
