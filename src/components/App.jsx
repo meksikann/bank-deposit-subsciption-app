32620d6ebf8abaa47eab6b32be6184eb76fcd301
serhii <meksikann@gmail.com>
2019-12-19 16:02:50 +0200

    sentiment UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,80 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Home from './Home';
+
+const getEmojiVal = (num) => {
+  if(num > 0.4) {
+    return 1
+  }
+
+  if(num < -0.3) {
+    return -1
+  }
+
+  return 0
+};
 
 export default class AppWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text:'',
+      rows:[],
+      disable: false
+    };
+
+    this.handleTExtChange = this.handleTExtChange.bind(this);
+    this.sendFeedback = this.sendFeedback.bind(this);
+  }
+
+
+  async sendFeedback() {
+    this.setState({
+      disable: true
+    });
+    const {text, rows} = this.state;
+
+    const response = await axios.get('http://localhost:8282/sentiment', {params: {text}});
+    console.log('Result:', response);
+    let emoji = getEmojiVal(response.data.score);
+
+    const score = `% ${Math.abs(response.data.score.toFixed(2) * 100)}`;
+    const magn = `% ${Math.abs(response.data.magn.toFixed(2) * 100)}`;
+    rows.push(this.createData(response.data.text, emoji, score, magn));
+    this.setState({
+      rows,
+      text:'',
+      disable: false
+    })
+  }
+
+  handleTExtChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  createData(text, emoji, score, magn) {
+    return { text, emoji, score, magn };
+  }
+
   render() {
+    const {rows, text, disable} = this.state;
+
     return (
       <div className='app-container'>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/about'}>About</Link>
-        <Link to={'/about/subroute'}>Subcomponent</Link>
-        {this.props.children}
+        {/*<Link to={'/'}>Home</Link>*/}
+        {/*<Link to={'/about'}>About</Link>*/}
+        {/*<Link to={'/about/subroute'}>Subcomponent</Link>*/}
+        {/*{this.props.children}*/}
+        <Home
+            handleClick={this.sendFeedback}
+            handleTExtChange={this.handleTExtChange}
+            data={rows}
+            textValue={text}
+            disable={disable}
+        />
       </div>
     )
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,101 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
+import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
 
-const Home = (props) => (
+
+const StyledTableCell = withStyles(theme => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default,
+        },
+    },
+}))(TableRow);
+
+
+
+const Home = ({handleClick, handleTExtChange, data, textValue, disable}) => (
   <div className='home'>
-    Home
+      <div className='col'>
+          <div><h3 style={{marginTop:'150px', marginLeft:'200px'}}>Feedback analyzer:</h3></div>
+          <div>
+              <TextField
+                  id="outlined-multiline-static"
+                  label="Please type feedback"
+                  multiline
+                  rows="4"
+                  variant="outlined"
+                  style={{margin: '20px', width:'600px'}}
+                  onChange={handleTExtChange}
+                  value={textValue}
+              />
+          </div>
+          <Button
+              variant="contained"
+              color='primary'
+              style={{margin:'20px', width:'100px'}}
+              onClick={handleClick}
+              disabled={disable}
+          >
+              Send
+          </Button>
+          {disable && <div>
+              <LinearProgress variant="query" />
+              <LinearProgress variant="query" color="secondary" />
+          </div>}
+
+          <div><h3 style={{marginLeft:'200px'}}>Sentiment analysis results:</h3></div>
+          <div style={{margin: '20px'}}>
+              <TableContainer component={Paper}>
+                  <Table  aria-label="customized table">
+                      <TableHead>
+                          <TableRow>
+                              <StyledTableCell align="left">Text</StyledTableCell>
+                              <StyledTableCell align="right"> Emoji</StyledTableCell>
+                              <StyledTableCell align="right">Score</StyledTableCell>
+                              <StyledTableCell align="right">Magnitude</StyledTableCell>
+                          </TableRow>
+                      </TableHead>
+                      <TableBody>
+                          {data.map(row => (
+                              <StyledTableRow key={row.text}>
+                                  <StyledTableCell align="left">{row.text}</StyledTableCell>
+                                  <StyledTableCell align="right">
+                                      {row.emoji === 1 && <SentimentVerySatisfiedIcon color='primary'/>}
+                                      {row.emoji === 0 && <SentimentSatisfiedIcon color='action'/>}
+                                      {row.emoji === -1 && <SentimentVeryDissatisfiedIcon color='error'/>}
+                                  </StyledTableCell>
+                                  <StyledTableCell align="right">{row.score}</StyledTableCell>
+                                  <StyledTableCell align="right">{row.magn}</StyledTableCell>
+                              </StyledTableRow>
+                          ))}
+                      </TableBody>
+                  </Table>
+              </TableContainer>
+          </div>
+      </div>
   </div>
-)
+);
 
 export default Home;
